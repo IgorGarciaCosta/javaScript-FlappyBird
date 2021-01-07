c32a4c0bf570a2aa50f8cca351d16cf483a4c1c8
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-07 11:06:56 -0300

    adicionando sprite do passaro

--- logic.js
+++ logic.js
@@ -3,9 +3,11 @@ const bird = new Image()
 bird.src= "./Img/bird.png"
 birdX = 0;
 interval = 24;
+birdSize = 24
 birdY = 200;
 canvasSize = 400;
 setInterval(()=>{
     context.fillStyle = 'skyblue';//pinta o canvas todo desta cor
-    context.fillRect(0, 0, canvasSize, canvasSize)
+    context.fillRect(0, 0, canvasSize, canvasSize);
+    context.drawImage(bird, birdX, birdY, birdSize * (524/374), birdSize)
 }, interval)
