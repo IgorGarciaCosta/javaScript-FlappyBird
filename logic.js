bd41e3ff81b281eeb54eec43067106ed8c8a8f11
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-07 11:09:32 -0300

    criação da gravidade

--- logic.js
+++ logic.js
@@ -5,9 +5,11 @@ birdX = 0;
 interval = 24;
 birdSize = 24
 birdY = 200;
+birdDY = 0;//deltaY é a velocidade vertical dele
 canvasSize = 400;
 setInterval(()=>{
     context.fillStyle = 'skyblue';//pinta o canvas todo desta cor
     context.fillRect(0, 0, canvasSize, canvasSize);
+    birdY -= birdDY -=0.5;//0.5 é a gravidade, basicamente
     context.drawImage(bird, birdX, birdY, birdSize * (524/374), birdSize)
 }, interval)
