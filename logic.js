613be8ee30cf2510decd56dddb776cbb945291eb
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-08 09:51:23 -0300

    gap aleatório entre os canos

--- logic.js
+++ logic.js
@@ -13,7 +13,7 @@ pipeWidth = 24;
 pipeGapBottomY = 24;
 pipeGap = 200;
 topPipeBottomY = 200;
-score =0;
+score = 0;
 bestScore = 9;
 
 c.onclick = () => (birdDY = 9)//faz ele subir ao clicar
@@ -28,15 +28,17 @@ setInterval(() => {
     if (pipeX < -pipeWidth) {//se o cano está fora da tela
         pipeX = canvasSize;
         topPipeBottomY = pipeGap * Math.random();
+
+        pipeGap = Math.floor(Math.random() * (200 - 70 + 1) + 70);
     }
     context.fillRect(pipeX, 0, pipeWidth, topPipeBottomY);//desenha o cano de cima (x, y, width, height)
     context.fillRect(pipeX, topPipeBottomY + pipeGap, pipeWidth, canvasSize);//desenha o cano de baixo
     context.fillStyle = 'black';
     context.fillText(score++, 9, 25);//(text, x, y, maxwidth)
-    bestScore = bestScore<score?score:bestScore;
+    bestScore = bestScore < score ? score : bestScore;
     context.fillText(`Melhor Pontuação: ${bestScore}`, 9, 50);
     (((birdY < topPipeBottomY || birdY > topPipeBottomY + pipeGap) //passaro acertou o cano/
-    && pipeX < birdSize * (524 / 374)) || birdY > canvasSize) &&//passaro caiu da tela
-        ((birdDY = 0), (birdY = 200), (pipeX = canvasSize), score =0);//pássaro morreu
+        && pipeX < birdSize * (524 / 374)) || birdY > canvasSize) &&//passaro caiu da tela
+        ((birdDY = 0), (birdY = 200), (pipeX = canvasSize), score = 0);//pássaro morreu
 
 }, interval)
